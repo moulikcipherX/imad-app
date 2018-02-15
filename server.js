e749cd14fa6ac77c6b18715b8a9e6e254b4d6235
Moulik Aggarwal <qwertymoulik@gmail.com>
2018-02-15 18:36:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,17 +9,68 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
-  res.send("The article-one  will be updated soon");
+app.get('/:articleName', function (req, res) {
+    let articleName = req.params.articleName;
+    res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article-two', function (req, res) {
-  res.send("The article-two  will be updated soon");
-});
+let articles = {
+    'article-one' : {
+        'title' : 'Moulik Aggarwal | Coding 101',
+        'heading':'Article One',
+        'content' : '<p> Moulik Aggarwals Content',
+        'date' : 'Feb 13 2018'
+    },
+    'article-two' : {
+        'title' : 'Moulik Aggarwal | Coding 102',
+        'heading':'Article Two',
+        'content' : '<p> Moulik Aggarwals Content',
+        'date' : 'Feb 15 2018'
+    },
+    'article-three' : {
+        'title' : 'Moulik Aggarwal | Coding 103',
+        'heading':'Article Three',
+        'content' : '<p> Moulik Aggarwals Content',
+        'date' : 'Feb 17 2018'
+    },
+};
 
-app.get('/article-three', function (req, res) {
-  res.send("The article-three  will be updated soon");
-});
+function createTemplate(data){
+    let title = data.title;
+    let heading = data.heading;
+    let content = data.content;
+    let date = data.date;
+    
+    let htmlTemplate =
+    `<html>
+        <head>
+            <title>${title}</title>
+            <meta name="viewport" content = "width = device-width" initial scale = 0 />
+            <link href="/ui/style.css" rel="stylesheet"/>
+        </head>
+        <body>
+            <div class="container">
+                <div>
+                    <a href="/">Home</a>
+                </div>
+                <hr/>
+            
+                <h3>
+                    ${heading}
+                </h3>
+                
+                <div>
+                    ${date}
+                </div>
+                
+                <div>
+                    ${content}
+                </div>
+            </div>
+        </body>
+    </html>`;
+    return htmlTemplate;
+}
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
