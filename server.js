ec87a24128bc38abd88c10c036f8d2a39c814dfd
Moulik Aggarwal <qwertymoulik@gmail.com>
2018-02-15 17:19:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function (req, res) {
+  res.sendFile("The article-one  will be updated soon");
+});
+
+app.get('/article-two', function (req, res) {
+  res.sendFile("The article-two  will be updated soon");
+});
+
+app.get('/article-three', function (req, res) {
+  res.sendFile("The article-three  will be updated soon");
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
