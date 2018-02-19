cf1777106da80809396f044258fc146cf7d734fd
Moulik Aggarwal <qwertymoulik@gmail.com>
2018-02-19 15:58:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,18 +27,36 @@ button.onclick = function()
     request.send(null);
 };
     //Submit name
-    var nameInput = document.getElementById('name');
-    var val = nameInput.value;
+    
     var submit = document.getElementById('submit_btn');
     submit.onclick = function() {
-        //Make a request and send a name
-        //Capture the list of names and render it as a list
         
-        var names = ['names1','name2'];
-        var list = '';
-        for(var i=0;i<names.length ; i++){
-            list += '<li>'+ names[i] +'</li>';
+    //Create A request object
+    var request = new XMLHttpRequest();
+    
+    //Capture The response and store it in the variable
+    request.onreadystatechange = function(){
+        if (request.readyState === XMLHttpRequest.DONE ){
+            //Take Some Action
+            if(request.status === 200){
+                //Capture the list of names and render it as a list
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for(var i=0;i<names.length ; i++){
+                list += '<li>'+ names[i] +'</li>';
         }
         var ul = document.getElementById('namelist');
         ul.innerHTML = list;
+                
+            }
+        }
+        //Not Done Yet
     };
+    
+    //Make the request
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
+    request.open('GET','http://badshahmoulik.imad.hasura-app.io/submit?name='+name,true);
+    request.send(null);
+};
