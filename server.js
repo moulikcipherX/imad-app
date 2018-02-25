3864402d2a0c8802749f60c2c0236476d4eb8590
Moulik Aggarwal <qwertymoulik@gmail.com>
2018-02-25 19:38:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -107,9 +107,23 @@ app.get('/submit',function (req,res){
     res.send(JSON.stringify(namelist));
 });
 
-app.get('/:articleName', function (req, res) {
-    var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+app.get('/article/:articleName', function (req, res) {
+    //var articleName = req.params.articleName;
+    
+    pool.query("SELECT * FROM article WHERE title ="+ req.params.articleName,function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else
+        {
+            if(result.rows.length === 0){
+                res.status(404).send('Article Not Found');
+            } else {
+                var articleData = result.row[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+    });
 });
 
 app.get('/ui/style.css', function (req, res) {
